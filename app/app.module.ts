4b7d0edf6b862e0c8c6f8851239f22d3cba77ef1
Riley Boyd <contact@rileyboyd.com>
2018-05-25 11:12:49 -0400

    Chapter 2, video 2 complete (Component Metadata).

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,41 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HttpXhrBackend } from '@angular/common/http';
-
+import { NgModule } from '@angular/core'; // Import module for decorators
+import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { MediaItemComponent } from './media-item.component';
-import { MediaItemListComponent } from './media-item-list.component';
-import { FavoriteDirective } from './favorite.directive';
-import { CategoryListPipe } from './category-list.pipe';
-import { MediaItemFormComponent } from './media-item-form.component';
-import { MediaItemService } from './media-item.service';
-import { lookupListToken, lookupLists } from './providers';
-import { MockXHRBackend } from './mock-xhr-backend';
-import { routing } from './app.routing';
 
 @NgModule({
   imports: [
-    BrowserModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    routing
+    BrowserModule
   ],
   declarations: [
-    AppComponent,
-    MediaItemComponent,
-    MediaItemListComponent,
-    FavoriteDirective,
-    CategoryListPipe,
-    MediaItemFormComponent
-  ],
-  providers: [
-    MediaItemService,
-    { provide: lookupListToken, useValue: lookupLists },
-    { provide: HttpXhrBackend, useClass: MockXHRBackend }
+    AppComponent
   ],
   bootstrap: [
     AppComponent
   ]
-})
+}) // No semicolon
+
 export class AppModule {}
