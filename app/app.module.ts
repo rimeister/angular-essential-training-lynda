74a7e87cea787ee6dedb72ef8e97ab5f7d6a2962
Riley Boyd <contact@rileyboyd.com>
2018-05-25 11:28:03 -0400

    Chapter 2, Video 3 complete

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'; // Import module for decorators
+import { NgModule } from '@angular/core'; // Import module for decorators. This is importing class types.
 import {BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,7 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; // Importing functions, whereas before we imported class types
 import { AppModule } from './app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic().bootstrapModule(AppModule); // Returns an instance of platform object. Has method named bootstrap module
+// Pass in AppModule type into the bootstrap module call
+
+// Use `npm start` in command line to start application
